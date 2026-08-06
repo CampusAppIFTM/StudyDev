fe614c54deb6f8cbc7a559d0d2f46d0e7807b2ce
felipepxl <felipeplopes2406@gmail.com>
2026-08-06 16:39:16 -0300

    Armazenando o objeto ‘user' com as informações do usuário logado

--- android/app/.cxx/RelWithDebInfo/25483bzj/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::hermesvm)
-add_library(hermes-engine::hermesvm SHARED IMPORTED)
-set_target_properties(hermes-engine::hermesvm PROPERTIES
-    IMPORTED_LOCATION "C:/Users/USER-2026/.gradle/caches/9.3.1/transforms/432f1effb5d7767355ba26d6d2efffdf/workspace/transformed/hermes-android-250829098.0.14-release/prefab/modules/hermesvm/libs/android.arm64-v8a/libhermesvm.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/USER-2026/.gradle/caches/9.3.1/transforms/432f1effb5d7767355ba26d6d2efffdf/workspace/transformed/hermes-android-250829098.0.14-release/prefab/modules/hermesvm/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
